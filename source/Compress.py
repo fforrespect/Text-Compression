2f819d92060fdb1e9cdeeac3fa0295f138db3f3e
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-03-10 00:21:02 +0000

    Tree now created

--- source/Compress.py
+++ source/Compress.py
@@ -1,12 +1,37 @@
 from Meta.Process import get_freq_dict
 from Meta import Constants as c, File
+from Tree.HuffmanTree import TreeNode
 
 
-def run():
+def _sort_tree_nodes(tree_nodes: list[TreeNode]) -> list[TreeNode]:
+	return sorted(tree_nodes, key=lambda item: item.frequency)
+
+
+def run() -> None:
 	file_content = File.get_content(c.INPUT_FILE_PATH)
 	
 	char_freqs: dict[str, int] = get_freq_dict(file_content)
-	sorted_char_freqs: list[tuple[str, int]] = [(k, v) for k, v in sorted(char_freqs.items(), key=lambda item: item[1])]
+	tree_nodes: list[TreeNode] = _sort_tree_nodes(
+		[TreeNode(char, freq)
+		 for char, freq in char_freqs.items()]
+	)
+
+	# for i in tree_nodes:
+	# 	print(i)
 	
-	print(sorted_char_freqs)
-	
+	while len(tree_nodes) > 1:
+		# get the two nodes with the smallest frequency
+		base_nodes: tuple[TreeNode, TreeNode] = tuple[TreeNode, TreeNode](tree_nodes[:2])
+		# work out the new character and frequency values
+		new_values: tuple[str, int] = ("".join(map(lambda x: x.char, base_nodes)),
+		                               sum(map(lambda x: x.frequency, base_nodes)))
+		# create a new node with those values and add it to the list
+		tree_nodes.append(TreeNode(*new_values, children=base_nodes))
+		# remove the old nodes from the list
+		tree_nodes.remove(base_nodes[0])
+		tree_nodes.remove(base_nodes[1])
+		# make sure the tree nodes list is sorted
+		tree_nodes = _sort_tree_nodes(tree_nodes)
+		
+	print(tree_nodes[0])
+		
--- source/Tree/HuffmanTree.py
+++ source/Tree/HuffmanTree.py
@@ -2,10 +2,13 @@ from typing import Optional
 
 
 class TreeNode:
-	def __init__(self, char: str, children: tuple[Optional[object], Optional[object]]):
+	def __init__(self, char: str, frequency: int, children: tuple[Optional[object], Optional[object]] = (None, None)):
 		self.char: str = char
+		self.frequency: int = frequency
 		self.children: tuple[Optional[TreeNode], Optional[TreeNode]] = children
 
+	def __str__(self) -> str:
+		return f"{repr(self.char)[1:-1].ljust(2, " ")}: {self.frequency}"
 
 # class Tree:
 # 	def __init__(self):
